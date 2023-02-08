4a9c7712bb98a1f4b46ba0d720d1e97236beee1e
Robin <rgourgues31@gmail.com>
2023-02-08 09:56:21 +0100

    new name

--- src/main/java/fr/summerdev/shifumi/Controller.java
+++ src/main/java/fr/summerdev/shifumi/Controller.java
@@ -3,12 +3,12 @@ package fr.summerdev.shifumi;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Pierre!" + "Feuill!" + "Ciseaux");
+        welcomeText.setText("Pierre!" + "Feuille!" + "Ciseaux");
     }
 }
